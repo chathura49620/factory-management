48bed80c20013b6f979dd43f183e36847bd10e4f
chathura49620 <chathuraprabath49620@gmail.com>
2021-08-12 22:53:56 +0530

    sprint1:category crud in backend

--- factory-managment-backend/server/model/category.model.js
+++ factory-managment-backend/server/model/category.model.js
@@ -1,16 +1,10 @@
 const mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
-    name:{
+    categoryName:{
         type:String,
         required:true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    gender:String,
     status:String,
 })
 
--- factory-managment-backend/server/routes/router.js
+++ factory-managment-backend/server/routes/router.js
@@ -8,7 +8,7 @@ const contoller = require('../controller/controller');
 //Super Admin Category APIs
 route.post('/api/categories',contoller.category.create);
 route.get('/api/categories',contoller.category.find);
-route.put('/api/categories/:id',contoller.category.update);
-route.delete('/api/categories/:id',contoller.category.delete);
+route.put('/api/categories/',contoller.category.update);
+route.delete('/api/categories/',contoller.category.delete);
 
 module.exports =route;
