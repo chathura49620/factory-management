8d7dad19c01200fec24e09fc3d81ac98c250f660
chathura49620 <chathuraprabath49620@gmail.com>
2021-08-15 13:03:12 +0530

    front end side bar

--- factory-managment-frontend/src/App.js
+++ factory-managment-frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/SuperAdmin/sideNav/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Overview from './pages/SuperAdmin/Overview';
+import { Reports, ReportsOne, ReportsTwo, ReportsThree } from './pages/SuperAdmin/Reports';
+import Team from './pages/SuperAdmin/Team';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Sidebar />
+      <Switch>
+        <Route path='/overview' exact component={Overview} />
+        <Route path='/reports' exact component={Reports} />
+        <Route path='/reports/reports1' exact component={ReportsOne} />
+        <Route path='/reports/reports2' exact component={ReportsTwo} />
+        <Route path='/reports/reports3' exact component={ReportsThree} />
+        <Route path='/team' exact component={Team} />
+      </Switch>
+    </Router>
   );
 }
 
