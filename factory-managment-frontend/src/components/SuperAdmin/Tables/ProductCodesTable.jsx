a0991e3391c4f585da0df3a00a94f7c9726b4a4b
chathura49620 <chathuraprabath49620@gmail.com>
2021-08-21 11:13:14 +0530

    sprint1:product code frontend

--- factory-managment-frontend/src/components/SuperAdmin/Tables/ProductCodesTable.jsx
+++ factory-managment-frontend/src/components/SuperAdmin/Tables/ProductCodesTable.jsx
@@ -1,6 +1,30 @@
 import React, { Component } from "react";
-const ProductCodesTable = ({ filteredItems}) => {
+import { Table, Button, ButtonToolbar } from 'react-bootstrap';
+import { EditProductCodeModal } from '../Modals/EditProductCodeModal';
+
+
+export class ProductCodesTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { cate: [], editModalShow: false }
+  }
+
+
+  render(){
+    const {id, productCode, productCategory, status } = this.state;
+    let EditModelClose = () => this.setState({ editModalShow: false })
   return (
+    <div>
+      <ButtonToolbar>
+      <EditProductCodeModal
+          show={this.state.editModalShow} 
+          onHide={EditModelClose}
+          id={id}
+          productCode={productCode}
+          productCategory={productCategory}
+          status={status}
+      />
+      </ButtonToolbar>
     <table className="table table-bordered table-sm m-2">
       <thead>
         <tr className="table-secondary">
@@ -11,21 +35,29 @@ const ProductCodesTable = ({ filteredItems}) => {
         </tr>
       </thead>
       <tbody>
-        {filteredItems.map((i) => (
+        {this.props.filteredItems.map((i) => (
           <tr
             key={i._id}
             className={
               "table-succes table-primary"
             }
-          >
-            <td>{i.userRoleNo}</td>
-            <td>{i.categoryName}</td>
+          > 
             <td></td>
+            <td>{i.productCode}</td>
+            <td>{i.productCategory}</td>
+            <td><button 
+            className="btn btn-success btn-sm"
+            onClick={() => this.setState({ editModalShow: true, id: i._id, productCode: i.productCode, productCategory: i.productCategory, status: i.status })}
+            >Edit</button> 
+            <button 
+            className="btn btn-warning btn-sm" 
+            >Delete</button></td>
           </tr>
         ))}
       </tbody>
     </table>
+    </div>
   );
 };
+};
 
-export default ProductCodesTable;
