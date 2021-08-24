44d1cc879bb7210493967a8bfbc4ab5594a6e995
chathura49620 <chathuraprabath49620@gmail.com>
2021-08-24 22:54:36 +0530

    sprint1:dasboard rendaring

--- factory-managment-frontend/src/App.js
+++ factory-managment-frontend/src/App.js
@@ -24,84 +24,12 @@ import Dashboard from "./pages/Dashboard";
 import AddNewProductionRound from "./pages/ProductionManager/AddNewProductionRound";
 import VIewNewProductionRound from "./pages/ProductionManager/VIewNewProductionRound";
 
+
 function App() {
   return (
-<<<<<<< HEAD
-    <Router>
-            <SuperAdminSideNav />
-            {/* <NavBar /> */}
-    <main className="container">
-      <Switch>
-          <Route path='/super-admin-dashboard' exact component={SuperAdminDashboard} />
-          <Route path='/basic-info' exact component={BasicInfo} />
-          <Route path='/user-roles' exact component={UserRoles} />
-          <Route path='/user-list' exact component={UserList} />
-          <Route path='/categories' exact component={Categories} />
-          <Route path='/product-codes' exact component={ProductCodes} />
-          <Route path='/metirial-codes' exact component={MaterialCodes} />
-          <Route path="/smdashboard" component={SMDashBoard}></Route>
-          <Route path="/myprofile" component={MyProfile}></Route>
-          <Route path="/items/:id" component={NewItemForm}></Route>
-          <Route path="/items" component={Item}></Route>
-          <Route path="/itemsrecords" component={ItemRecord}></Route>
-          {/* <Route path="/notfound" component={NotFound}></Route> */}
-
-          //employee routes
-          <Route path= '/employee-dashboard' exact component={EmployeeDashboard} />
-          <Route path= '/assignments' exact component={Assignments}/>
-          <Route path= '/calendar' exact component={Calendar}/>
-          <Route path= '/profile' exact component={Profile}/>
-          <Route path= '/payments' exact component={Payments}/>
-          <Route path= '/leave' exact component={Leave}/>
-=======
-
-
       <Dashboard />
-    // <Router>
-    //         <SuperAdminSideNav />
-    //         {/* <NavBar /> */}
-    // <main className="container">
-    //   <Switch>
-    //       <Route path='/login' exact component={login} />
-    //       <Route path='/super-admin-dashboard' exact component={SuperAdminDashboard} />
-    //       <Route path='/basic-info' exact component={BasicInfo} />
-    //       <Route path='/user-roles' exact component={UserRoles} />
-    //       <Route path='/user-list' exact component={UserList} />
-    //       <Route path='/categories' exact component={Categories} />
-    //       <Route path='/product-codes' exact component={ProductCodes} />
-    //       <Route path='/metirial-codes' exact component={MaterialCodes} />
-    //       <Route path="/smdashboard" component={SMDashBoard}></Route>
-    //       <Route path="/myprofile" component={MyProfile}></Route>
-    //       <Route path="/items/:id" component={NewItemForm}></Route>
-    //       <Route path="/items" component={Item}></Route>
-    //       <Route path="/itemsrecords" component={ItemRecord}></Route>
-    //       {/* <Route path="/notfound" component={NotFound}></Route> */}
-
-    //       {/* employee routes */}
-    //       <Route path= '/employee-dashboard' exact component={EmployeeDashboard} />
-    //       <Route path= '/assignments' exact component={Assignments}/>
-    //       <Route path= '/calendar' exact component={Calendar}/>
-    //       <Route path= '/profile' exact component={Profile}/>
-    //       <Route path= '/payments' exact component={Payments}/>
-
-    //       <Route path= '/leave' exact component={Leave}/>
-
-
-    //       {/* production manager routers */}
-    //       {/* <Route path= '/add-new-production-round' exact component={AddNewProductionRound} />
-    //       <Route path= '/view-production-rounds' exact component={VIewNewProductionRound}/> */}
-
-
-
->>>>>>> ac31f3102674b83ea50610c76ee18218a901b208
-         
-       
-
-    //       {/* <Redirect to="/notfound"></Redirect> */}
-    //     </Switch>
-    //   </main>
-    // </Router>
-  );
+   );
+  
 }
 
 export default App;
--- factory-managment-frontend/src/pages/Dashboard.jsx
+++ factory-managment-frontend/src/pages/Dashboard.jsx
@@ -51,28 +51,33 @@ class Dashboard extends Component {
     }
 
     renderSideNavigation(){
-        const user_role = localStorage.getItem('user_role')
-        if(user_role == 'employee'){
-            return (
-                <EmpsideNav/>   
-            );
-        }
-        if(user_role == 'stock member'){
-            return (
-                <h1></h1>
-            );
-        }
-        if(user_role == 'production team member'){
-            return (
-                <ProManagerNav />
-            );
-        }
-        if(user_role == 'Super Admin'){
-            return (
-                <SuperAdminSideNav/> 
-            );
+        const user_role = localStorage.getItem('user_role');
+        const is_login = localStorage.getItem('is_login');
+        if(is_login != '1'){
+                return false;
         }else{
-            return false;
+            if(user_role == 'employee'){
+                return (
+                    <EmpsideNav/>   
+                );
+            }
+            if(user_role == 'stock member'){
+                return (
+                    <h1></h1>
+                );
+            }
+            if(user_role == 'production team member'){
+                return (
+                    <ProManagerNav />
+                );
+            }
+            if(user_role == 'Super Admin'){
+                return (
+                    <SuperAdminSideNav/> 
+                );
+            }else{
+                return false;
+            }
         }
     }
 
@@ -84,58 +89,59 @@ class Dashboard extends Component {
         if(is_login != '1'){
             return (
                 <div>
-                    <Route path= '/login' exact component={login} />
-                </div>
-            );
-        }
-        if(user_role == 'employee'){
-            return (
-                <div>
-                    <Route path= '/employee-dashboard' exact component={EmployeeDashboard} />
-                    <Route path= '/assignments' exact component={Assignments}/>
-                    <Route path= '/calendar' exact component={Calendar}/>
-                    <Route path= '/profile' exact component={Profile}/>
-                    <Route path= '/payments' exact component={Payments}/> 
-                    <Route path= '/leave' exact component={Leave}/> 
-                </div>
-            );
-        }if(user_role == 'Super Admin'){
-            return (
-                <div>
-                    <Route path='/super-admin-dashboard' exact component={SuperAdminDashboard} />
-                    <Route path='/basic-info' exact component={BasicInfo} />
-                    <Route path='/user-roles' exact component={UserRoles} />
-                    <Route path='/user-list' exact component={UserList} />
-                    <Route path='/categories' exact component={Categories} />
-                    <Route path='/product-codes' exact component={ProductCodes} />
-                    <Route path='/metirial-codes' exact component={MaterialCodes} />
-                </div>
-            );
-        }if(user_role == 'production team member'){
-            return (
-                <div>
-                    <div>
-                        <Route path= '/add-new-production-round' exact component={AddNewProductionRound} />
-                        <Route path= '/view-production-rounds' exact component={VIewNewProductionRound}/>
-                        <Route path= '/view-product-list' exact component={ViewProductList}/>
-                    </div>
-                </div>
-            );
-        }if(user_role == 'stock member'){
-            return (
-                <div>
-                    <div>
-                    <Route path="/smdashboard" component={SMDashBoard}></Route>
-                    <Route path="/myprofile" component={MyProfile}></Route>
-                    <Route path="/items/:id" component={NewItemForm}></Route>
-                    <Route path="/items" component={Item}></Route>
-                    <Route path="/itemsrecords" component={ItemRecord}></Route>
-                    {/* <Route path="/notfound" component={NotFound}></Route> */}
-                    </div>
+                    <Route path= '/' exact component={login} />
                 </div>
             );
         }else{
-            return false;
+            if(user_role == 'employee'){
+                return (
+                    <div>
+                        <Route path= '/' exact component={EmployeeDashboard} />
+                        <Route path= '/assignments' exact component={Assignments}/>
+                        <Route path= '/calendar' exact component={Calendar}/>
+                        <Route path= '/profile' exact component={Profile}/>
+                        <Route path= '/payments' exact component={Payments}/> 
+                        <Route path= '/leave' exact component={Leave}/> 
+                    </div>
+                );
+            }if(user_role == 'Super Admin'){
+                return (
+                    <div>
+                        <Route path='/' exact component={SuperAdminDashboard} />
+                        <Route path='/basic-info' exact component={BasicInfo} />
+                        <Route path='/user-roles' exact component={UserRoles} />
+                        <Route path='/user-list' exact component={UserList} />
+                        <Route path='/categories' exact component={Categories} />
+                        <Route path='/product-codes' exact component={ProductCodes} />
+                        <Route path='/metirial-codes' exact component={MaterialCodes} />
+                    </div>
+                );
+            }if(user_role == 'production team member'){
+                return (
+                    <div>
+                        <div>
+                            <Route path= '/add-new-production-round' exact component={AddNewProductionRound} />
+                            <Route path= '/view-production-rounds' exact component={VIewNewProductionRound}/>
+                            <Route path= '/view-product-list' exact component={ViewProductList}/>
+                        </div>
+                    </div>
+                );
+            }if(user_role == 'stock member'){
+                return (
+                    <div>
+                        <div>
+                        <Route path="/smdashboard" component={SMDashBoard}></Route>
+                        <Route path="/myprofile" component={MyProfile}></Route>
+                        <Route path="/items/:id" component={NewItemForm}></Route>
+                        <Route path="/items" component={Item}></Route>
+                        <Route path="/itemsrecords" component={ItemRecord}></Route>
+                        {/* <Route path="/notfound" component={NotFound}></Route> */}
+                        </div>
+                    </div>
+                );
+            }else{
+                return false;
+            }
         }
     }
 
