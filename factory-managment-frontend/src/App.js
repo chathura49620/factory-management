fb38db581c18901c5e3abaf571044a335952f694
Rashmika564 <rashmikarajapaksha228@gmail.com>
2021-09-06 09:52:49 +0530

    modified app.js and dashboard.jsx due to deletions

--- factory-managment-frontend/src/App.js
+++ factory-managment-frontend/src/App.js
@@ -1,12 +1,4 @@
 import "./App.css";
-import login from "./pages/SuperAdmin/login";
-
-import EmployeeDashboard from "./pages/Employee/EmployeeDashboard";
-import Assignments from "./pages/Employee/Assignments";
-import Calendar from "./pages/Employee/Calendar";
-import Leave from "./pages/Employee/Leave";
-import Payments from "./pages/Employee/Payments";
-import Profile from "./pages/Employee/Profile";
 
 import Dashboard from "./pages/Dashboard";
 
--- factory-managment-frontend/src/pages/Dashboard.jsx
+++ factory-managment-frontend/src/pages/Dashboard.jsx
@@ -1,35 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-
-import SuperAdminSideNav from '../components/SuperAdmin/sideNav/Sidebar';
-import EmpsideNav from '../components/Employee/EmpsideNav/EmpSidebar';
-import ProManagerNav from '../components/ProductionManager/ProManagersideNav/EmpSidebar';
-import FinanceTeamMemberSideNav from '../components/FinanceTeamMember/sideNav/Sidebar';
-
-
-import ProductionManagerDashboard from "./ProductionManager/ProManagerDashboard";
-
 import Sidebar from "../components/storeManager/sideNavigation/Sidebar";
-
-import SuperAdminDashboard from "./SuperAdmin/Dashboard";
-import EmployeeDashboard from "./Employee/EmployeeDashboard";
-
-//super admin routers
-import BasicInfo from "./SuperAdmin/BasicInfo";
-import UserRoles from "./SuperAdmin/UserRoles";
-import UserList from "./SuperAdmin/UserList";
-import Categories from "./SuperAdmin/Categories";
-import ProductCodes from "./SuperAdmin/ProductCodes";
-import MaterialCodes from "./SuperAdmin/MaterialCodes";
-import login from "./SuperAdmin/login";
-
-//employee routes
-import Assignments from "./Employee/Assignments";
-import Calendar from "./Employee/Calendar";
-import Leave from "./Employee/Leave";
-import Profile from "./Employee/Profile";
-import Payments from "./Employee/Payments";
 
 //stock manager routers
 import SMDashBoard from "./storeManager/sm_dashboard";
@@ -37,12 +9,6 @@ import NewItemForm from "../components/storeManager/forms/newitemform";
 import ItemRecord from "./storeManager/itemsrecords";
 import MyProfile from "./storeManager/myprofile";
 import Item from "./storeManager/items";
-
-//production manager routes
-import AddNewProductionRound from "./ProductionManager/AddNewProductionRound";
-import VIewNewProductionRound from "./ProductionManager/VIewNewProductionRound";
-import ViewProductList from "./ProductionManager/ViewProductList";
-
 
 class Dashboard extends Component {
   constructor(props) {
@@ -55,22 +21,22 @@ class Dashboard extends Component {
   }
 
   renderSideNavigation() {
-    const user_role = localStorage.getItem("user_role");
+    const user_role = "stock member";
     const is_login = localStorage.getItem("is_login");
     if (is_login != "1") {
       return false;
     } else {
       if (user_role == "employee") {
-        return <EmpsideNav />;
+        // return <EmpsideNav />;
       }
       if (user_role == "stock member") {
         return <Sidebar />;
       }
       if (user_role == "production team member") {
-        return <ProManagerNav />;
+        // return <ProManagerNav />;
       }
       if (user_role == "Super Admin") {
-        return <SuperAdminSideNav />;
+        // return <SuperAdminSideNav />;
       } else {
         return false;
       }
@@ -78,66 +44,69 @@ class Dashboard extends Component {
   }
 
   renderDashboard() {
-    const user_role = localStorage.getItem("user_role");
+    const user_role = "stock member";
     const is_login = localStorage.getItem("is_login");
     if (is_login != "1") {
-      return (
-        <div>
-          <Route path="/" exact component={login} />
-        </div>
-      );
+      // return (
+      //   <div>
+      //     <Route path="/" exact component={login} />
+      //   </div>
+      // );
     } else {
       if (user_role == "employee") {
         return (
-          <div>
-            <Route path="/" exact component={EmployeeDashboard} />
-            <Route path="/assignments" exact component={Assignments} />
-            <Route path="/calendar" exact component={Calendar} />
-            <Route path="/profile" exact component={Profile} />
-            <Route path="/payments" exact component={Payments} />
-            <Route path="/leave" exact component={Leave} />
-          </div>
+          // <div>
+          //   <Route path="/" exact component={EmployeeDashboard} />
+          //   <Route path="/assignments" exact component={Assignments} />
+          //   <Route path="/calendar" exact component={Calendar} />
+          //   <Route path="/profile" exact component={Profile} />
+          //   <Route path="/payments" exact component={Payments} />
+          //   <Route path="/leave" exact component={Leave} />
+          // </div>
+          null
         );
       }
       if (user_role == "Super Admin") {
         return (
-          <div>
-            <Route path="/" exact component={SuperAdminDashboard} />
-            <Route path="/basic-info" exact component={BasicInfo} />
-            <Route path="/user-roles" exact component={UserRoles} />
-            <Route path="/user-list" exact component={UserList} />
-            <Route path="/categories" exact component={Categories} />
-            <Route path="/product-codes" exact component={ProductCodes} />
-            <Route path="/metirial-codes" exact component={MaterialCodes} />
-          </div>
+          // <div>
+          //   <Route path="/" exact component={SuperAdminDashboard} />
+          //   <Route path="/basic-info" exact component={BasicInfo} />
+          //   <Route path="/user-roles" exact component={UserRoles} />
+          //   <Route path="/user-list" exact component={UserList} />
+          //   <Route path="/categories" exact component={Categories} />
+          //   <Route path="/product-codes" exact component={ProductCodes} />
+          //   <Route path="/metirial-codes" exact component={MaterialCodes} />
+          // </div>
+          null
         );
       }
       if (user_role == "production team member") {
         return (
-          <div>
-            <div>
-              <Route
-                path="/"
-                exact
-                component={ProductionManagerDashboard}
-              ></Route>
-              <Route
-                path="/add-new-production-round"
-                exact
-                component={AddNewProductionRound}
-              />
-              <Route
-                path="/view-production-rounds"
-                exact
-                component={VIewNewProductionRound}
-              />
-              <Route
-                path="/view-product-list"
-                exact
-                component={ViewProductList}
-              />
-            </div>
-          </div>
+          // <div>
+          //   <div>
+          //     <Route
+          //       path="/"
+          //       exact
+          //       component={ProductionManagerDashboard}
+          //     ></Route>
+          //     <Route
+          //       path="/add-new-production-round"
+          //       exact
+          //       component={AddNewProductionRound}
+          //     />
+          //     <Route
+          //       path="/view-production-rounds"
+          //       exact
+          //       component={VIewNewProductionRound}
+          //     />
+          //     <Route
+          //       path="/view-product-list"
+          //       exact
+          //       component={ViewProductList}
+          //     />
+          //   </div>
+          // </div>
+          null
         );
       }
       if (user_role == "stock member") {
@@ -157,7 +126,6 @@ class Dashboard extends Component {
       } else {
         return false;
       }
-
     }
   }
 
