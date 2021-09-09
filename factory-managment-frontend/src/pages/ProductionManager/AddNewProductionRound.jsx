579b7d5034fdd26e583bf4f6d80f1ea8d1abdb74
Thiranya-Malaviarachchi <thiranyamalaviarachchi@gmail.com>
2021-09-09 17:41:51 +0530

    IT19129440 : Impelement Add New Production Round Page - Frontend

--- factory-managment-frontend/src/pages/ProductionManager/AddNewProductionRound.jsx
+++ factory-managment-frontend/src/pages/ProductionManager/AddNewProductionRound.jsx
@@ -103,9 +103,9 @@ validate(){
                                     <Form.Group controlId="proId">
                                         <Form.Label style={{fontWeight:"bold"}}>Product Id</Form.Label>
                                         <Form.Control style={{ border: "1px solid #050139" }}  type="text" name="proId"  placeholder="Product Id"  />
-                                        <div style={{background:"#f8d7da"}}>{this.state.validationError}</div>
+                                        {/* <div style={{background:"#f8d7da"}}>{this.state.validationError}</div> */}
                                     </Form.Group>
-                                        <Form.Label style={{fontWeight:"bold"}}>Product Category</Form.Label>
+                                        {/* <Form.Label style={{fontWeight:"bold"}}>Product Category</Form.Label> */}
                                         <Form.Control style={{ border: "1px solid #050139" }} as="select"  name="productCategory">
                                         <div style={{background:"#f8d7da"}}>{this.state.validationError}</div>
                                         {this.state.categories.map((i) => (
