5def035e4b518f4c85b8e65f5622f16653bf78cb
Thiranya-Malaviarachchi <thiranyamalaviarachchi@gmail.com>
2021-09-10 12:11:04 +0530

    IT19129440 - Implement Feedback Management Routes For Backend API

--- factory-managment-backend/server/routes/router.js
+++ factory-managment-backend/server/routes/router.js
@@ -84,7 +84,6 @@ route.put('/api/order-details/',contoller.orderDetails.update);
 route.delete('/api/order-details/',contoller.orderDetails.delete);
 
 //Feedback Details APIs
-
 route.post('/api/feedback-details/',contoller.feedbackDetails.create);
 route.get('/apifeedback-details/',contoller.feedbackdetails.find);
 
