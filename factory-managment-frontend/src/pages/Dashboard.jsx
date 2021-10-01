b501ef4ef5327624398e568bc64cd37ded739961
chathura49620 <chathuraprabath49620@gmail.com>
2021-10-01 16:54:13 +0530

    changes

--- factory-managment-frontend/src/pages/Dashboard.jsx
+++ factory-managment-frontend/src/pages/Dashboard.jsx
@@ -2,17 +2,17 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import SuperAdminSideNav from '../components/SuperAdmin/sideNav/Sidebar';
-import EmpsideNav from '../components/Employee/EmpsideNav/EmpSidebar';
-import ProManagerNav from '../components/ProductionManager/ProManagersideNav/EmpSidebar';
+// import EmpsideNav from '../components/Employee/EmpsideNav/EmpSidebar';
+// import ProManagerNav from '../components/ProductionManager/ProManagersideNav/EmpSidebar';
 import FinanceTeamMemberSideNav from '../components/FinanceTeamMember/sideNav/Sidebar';
 
-import ProductionManagerDashboard from './ProductionManager/ProManagerDashboard'
+// import ProductionManagerDashboard from './ProductionManager/ProManagerDashboard'
  
-import Sidebar from "../components/storeManagerComponents/sideNavigation/Sidebar";
+// import Sidebar from "../components/storeManagerComponents/sideNavigation/Sidebar";
  
 
 import SuperAdminDashboard from './SuperAdmin/Dashboard'
-import EmployeeDashboard from './Employee/EmployeeDashboard'
+// import EmployeeDashboard from './Employee/EmployeeDashboard'
  
 //super admin routers
 import BasicInfo from './SuperAdmin/BasicInfo';
@@ -23,27 +23,27 @@ import ProductCodes from './SuperAdmin/ProductCodes';
 import MaterialCodes from './SuperAdmin/MaterialCodes';
 import  login  from './SuperAdmin/login';
 
-//employee routes
-import Assignments from './Employee/Assignments';
-import Calendar from './Employee/Calendar';
-import Leave from './Employee/Leave';
-import Profile from './Employee/Profile';
-import Payments from './Employee/Payments';
+// //employee routes
+// import Assignments from './Employee/Assignments';
+// import Calendar from './Employee/Calendar';
+// import Leave from './Employee/Leave';
+// import Profile from './Employee/Profile';
+// import Payments from './Employee/Payments';
 
 
-//stock manager routers
-import SMDashBoard from "../components/storeManagerComponents/sm_dashboard";
-import NewItemForm from "../components/storeManagerComponents/newitemform";
-import ItemRecord from "../components/storeManagerComponents/itemsrecords";
-import MyProfile from "../components/storeManagerComponents/myprofile";
-import NavBar from "../components/storeManagerComponents/navbar";
-import Item from "../components/storeManagerComponents/items";
+// //stock manager routers
+// import SMDashBoard from "../components/storeManagerComponents/sm_dashboard";
+// import NewItemForm from "../components/storeManagerComponents/newitemform";
+// import ItemRecord from "../components/storeManagerComponents/itemsrecords";
+// import MyProfile from "../components/storeManagerComponents/myprofile";
+// import NavBar from "../components/storeManagerComponents/navbar";
+// import Item from "../components/storeManagerComponents/items";
 
 
-//production manager routes
-import AddNewProductionRound from "./ProductionManager/AddNewProductionRound";
-import VIewNewProductionRound from "./ProductionManager/VIewNewProductionRound";
-import ViewProductList from "./ProductionManager/ViewProductList";
+// //production manager routes
+// import AddNewProductionRound from "./ProductionManager/AddNewProductionRound";
+// import VIewNewProductionRound from "./ProductionManager/VIewNewProductionRound";
+// import ViewProductList from "./ProductionManager/ViewProductList";
 
 
 //finance team member routers
@@ -71,34 +71,37 @@ class Dashboard extends Component {
         if(is_login != '1'){
                 return false;
         }else{
-            if(user_role == 'employee'){
-                return (
-                    <EmpsideNav/>   
-                );
-            }
-            if(user_role == 'stock member'){
-                return (
-                    <NavBar />
-                );
-            }
-            if(user_role == 'production team member'){
-                return (
-                    <ProManagerNav />
-                );
-            }
+            // if(user_role == 'employee'){
+            //     return (
+            //         <EmpsideNav/>   
+            //     );
+            // }
+            // if(user_role == 'stock member'){
+            //     return (
+            //         <NavBar />
+            //     );
+            // }
+            // if(user_role == 'production team member'){
+            //     return (
+            //         <ProManagerNav />
+            //     );
+            // }
             if(user_role == 'Super Admin'){
                 return (
                     <SuperAdminSideNav/> 
                 );
-            }if(user_role == 'finance team member'){
+            }
+            if(user_role == 'finance team member'){
                 return (
                     <FinanceTeamMemberSideNav/>   
                 );
-            }if(user_role == 'Whole Sale Buyer'){
-                return (
-                    <FinanceTeamMemberSideNav/>   
-                );
-            }else{
+            }
+            // if(user_role == 'Whole Sale Buyer'){
+            //     return (
+            //         <FinanceTeamMemberSideNav/>   
+            //     );
+            // }
+            else{
                 return false;
             }
         }
@@ -116,18 +119,19 @@ class Dashboard extends Component {
                 </div>
             );
         }else{
-            if(user_role == 'employee'){
-                return (
-                    <div>
-                        <Route path= '/' exact component={EmployeeDashboard} />
-                        <Route path= '/assignments' exact component={Assignments}/>
-                        <Route path= '/calendar' exact component={Calendar}/>
-                        <Route path= '/profile' exact component={Profile}/>
-                        <Route path= '/payments' exact component={Payments}/> 
-                        <Route path= '/leave' exact component={Leave}/> 
-                    </div>
-                );
-            }if(user_role == 'Super Admin'){
+            // if(user_role == 'employee'){
+            //     return (
+            //         <div>
+            //             <Route path= '/' exact component={EmployeeDashboard} />
+            //             <Route path= '/assignments' exact component={Assignments}/>
+            //             <Route path= '/calendar' exact component={Calendar}/>
+            //             <Route path= '/profile' exact component={Profile}/>
+            //             <Route path= '/payments' exact component={Payments}/> 
+            //             <Route path= '/leave' exact component={Leave}/> 
+            //         </div>
+            //     );
+            // }
+            if(user_role == 'Super Admin'){
                 return (
                     <div>
                         <Route path='/' exact component={SuperAdminDashboard} />
@@ -139,7 +143,8 @@ class Dashboard extends Component {
                         <Route path='/metirial-codes' exact component={MaterialCodes} />
                     </div>
                 );
-                }if(user_role == 'finance team member'){
+                }
+                if(user_role == 'finance team member'){
                     return (
                         <div>
                             <Route path='/' exact component={FinancialAdminDashboard} />
@@ -152,40 +157,42 @@ class Dashboard extends Component {
                            
                         </div>
                     ); 
-                }if(user_role == 'production team member'){
-                return (
-                    <div>
-                        <div>
-                           <Route path= '/' exact component={ProductionManagerDashboard}/>
-                            <Route path= '/add-new-production-round' exact component={AddNewProductionRound} />
-                            <Route path= '/view-production-rounds' exact component={VIewNewProductionRound}/>
-                            <Route path= '/view-product-list' exact component={ViewProductList}/>
-                        </div>
-                    </div>
-                );
-            }if(user_role == 'stock member'){
-                return (
-                    <div>
-                        <div>
-                        <Route path="/" component={SMDashBoard}></Route>
-                        <Route path="/myprofile" component={MyProfile}></Route>
-                        <Route path="/items/:id" component={NewItemForm}></Route>
-                        <Route path="/items" component={Item}></Route>
-                        <Route path="/itemsrecords" component={ItemRecord}></Route>
-                        {/* <Route path="/notfound" component={NotFound}></Route> */}
-                        </div>
-                    </div>
-                );
-            }if(user_role == 'Whole Sale Buyer'){
-                return (
-                    <div>
-                        <div>
-                        <Route path= '/' exact component={ProductionManagerDashboard}/>
-                            <Route path= '/add-new-production-round' exact component={AddNewProductionRound} />
-                        </div>
-                    </div>
-                );
-            }else{
+                }
+            //     if(user_role == 'production team member'){
+            //     return (
+            //         <div>
+            //             <div>
+            //                <Route path= '/' exact component={ProductionManagerDashboard}/>
+            //                 <Route path= '/add-new-production-round' exact component={AddNewProductionRound} />
+            //                 <Route path= '/view-production-rounds' exact component={VIewNewProductionRound}/>
+            //                 <Route path= '/view-product-list' exact component={ViewProductList}/>
+            //             </div>
+            //         </div>
+            //     );
+            // }if(user_role == 'stock member'){
+            //     return (
+            //         <div>
+            //             <div>
+            //             <Route path="/" component={SMDashBoard}></Route>
+            //             <Route path="/myprofile" component={MyProfile}></Route>
+            //             <Route path="/items/:id" component={NewItemForm}></Route>
+            //             <Route path="/items" component={Item}></Route>
+            //             <Route path="/itemsrecords" component={ItemRecord}></Route>
+            //             {/* <Route path="/notfound" component={NotFound}></Route> */}
+            //             </div>
+            //         </div>
+            //     );
+            // }if(user_role == 'Whole Sale Buyer'){
+            //     return (
+            //         <div>
+            //             <div>
+            //             <Route path= '/' exact component={ProductionManagerDashboard}/>
+            //                 <Route path= '/add-new-production-round' exact component={AddNewProductionRound} />
+            //             </div>
+            //         </div>
+            //     );
+            // }
+            else{
                 return false;
             }
         }
